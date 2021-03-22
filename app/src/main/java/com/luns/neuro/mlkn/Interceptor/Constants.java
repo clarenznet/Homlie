1a34e154099d8fe317c865a67ea4756a6fc324b2
clarenznet <clarenznet@gmail.com>
2021-03-23 00:39:39 +0300

    Update Constants.java
    removed pass key

--- app/src/main/java/com/luns/neuro/mlkn/Interceptor/Constants.java
+++ app/src/main/java/com/luns/neuro/mlkn/Interceptor/Constants.java
@@ -10,7 +10,7 @@ public class Constants {
     public static final String BASE_URL = "https://api.safaricom.co.ke/";
 
     public static final String BUSINESS_SHORT_CODE = "504628";
-    public static final String PASSKEY = "ba95f91d4c495092444e625821fb00cc5eec70f8a4d64c0fdc95f8c23b501283";
+    public static final String PASSKEY = "";
     public static final String TRANSACTION_TYPE = "CustomerBuyGoodsOnline";//"CustomerPayBillOnline";
     public static final String PARTYB = "507984"; //174379same as 504628business shortcode above7099181
     public static final String CALLBACKURL = "https://www.homlie.co.ke/aqim/callback_url.php?strticketcode=";
